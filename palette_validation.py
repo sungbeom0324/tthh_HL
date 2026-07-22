881c3b5a419aff16baba573990010fb8e95fcdf9
Sungbeom_Cho <stiger97@hanyang.ac.kr>
2026-07-22 17:12:41 +0900

    Add validation codes

---
+++ palette_validation.py
@@ -0,0 +1,16 @@
+# Draw histograms with modules. conda activate py36 
+import sys; sys.path.insert(0, "/home/stiger97/github/tthh")
+from utils.drawHistoModules import *
+
+indir = "/home/stiger97/github/tthh/skimmed/test/"
+PRE = "test"
+tree = "Delphes"
+lumi = 3000  # Luminosity in fb^-1
+
+# Higgs mass
+indir = "/home/stiger97/github/tthh/skimmed/validation/"
+drawHistoSame_Val(indir, tree, "GenHiggs1_m", "H1-GenJet m_{bb} (GeV)", "Normalized Events", "GenHiggs1_m", 60, 0, 300, "skimmed", "S0", yscale=1.3)
+drawHistoSame_Val(indir, tree, "GenHiggs2_m", "H2-GenJet m_{bb} (GeV)", "Normalized Events", "GenHiggs2_m", 60, 0, 300, "skimmed", "S0", yscale=1.3)
+drawHistoSame_Val(indir, tree, "Matched_Higgs1_m", "H1-Gen-matched Reco m_{bb} (GeV)", "Normalized Events", "Matched_Higgs1_m", 60, 0, 300, "skimmed", "S0", yscale=1.3)
+drawHistoSame_Val(indir, tree, "Matched_Higgs2_m", "H2-Gen-matched Reco m_{bb} (GeV)", "Normalized Events", "Matched_Higgs2_m", 60, 0, 300, "skimmed", "S0", yscale=1.3)
+
---
+++ scripts/validation.sh
@@ -0,0 +1,8 @@
+root -l -b -q 'skimmer/ana_validation.C("fj_AA0_tthh.root")'
+root -l -b -q 'skimmer/ana_validation.C("fj_AA5_tthh.root")'
+
+#root -l -b -q 'skimmer/ana_validation.C("fj_tthh.root")'
+#root -l -b -q 'skimmer/ana_validation.C("fjcore_tthh.root")'
+#root -l -b -q 'skimmer/ana_validation.C("fjCMS_tthh.root")'
+#root -l -b -q 'skimmer/ana_validation.C("fjcoreCMS_tthh.root")'
+#python palette_validation.py
